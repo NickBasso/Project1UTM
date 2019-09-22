774ade0115f9d9c3424f29bdef57eed58db3e924
pascarustefanalin <pascarustefanalin@gmail.com>
2019-09-22 22:50:46 +0300

    Update newtPoly.c

--- newtPoly.c
+++ newtPoly.c
@@ -14,7 +14,7 @@ main(){
 	
 	fscanf(fp, "%d", &numElem);
 	
-	for(i = 1; i <= numElem; i++){
+	for(i = 0; i < numElem; i++){
 		fscanf(fp, "%f", &arr[i]);
 	}
 	
@@ -25,22 +25,22 @@ main(){
 
 void polynomNewtIntr()
 {
-    float p[10000], f=arr[1], f1=1, f2=0;
-    int k=numElem+1,i,j=1;
+    float p[10000], f=arr[0], f1=1, f2=0;
+    int k=numElem-1,i,j=1;
  
     do
     {
-        for (i=1;i<=numElem-1;i++)
+        for (i=0;i<numElem-1;i++)
         {
             p[i] = ((arr[i+1]-arr[i])/j);
             arr[i]=p[i];
         }
         f1=1;
-        for(i=1;i<=j;i++)
+        for(i=0;i<j;i++)
             {
-                f1*=(k-i);
+                f1*=(k-i+1);
             }
-        f2+=(arr[1]*f1);
+        f2+=(arr[0]*f1);
         numElem--;
         j++;
     }
