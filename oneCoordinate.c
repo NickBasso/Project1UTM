12e3712b5001c27c7fb7aa60bd01cfd9c41b5e26
Nick <NickBasso@users.noreply.github.com>
2019-09-22 23:24:43 +0300

    updated core solution & report in docx + pdf formats

--- oneCoordinate.c
+++ oneCoordinate.c
@@ -9,6 +9,9 @@ void linearDeltaCoefExtr();
 void conicSectionExtr();
 void polynomLagrExtr();
 
+// side task - Newton's polynomial interpolation algo implementation by Stefan-Alin Pascaru
+void polynomNewtIntr();
+
 // main declarations for global scope usage
 int numElem;			// number of elements
 float arr[10000 + 5];	// float-type array of 10000 + 5 elements 
@@ -38,6 +41,8 @@ main(){
 	linearDeltaCoefExtr();
 	conicSectionExtr();
 	polynomLagrExtr();
+	
+	polynomNewtIntr();
 	
 	return 0;
 }
@@ -159,3 +164,30 @@ void polynomLagrExtr(){
 	printf("Lagrange extrapolation result:\t\t\t\t\t       %f\n", result);
 }
 
+// side task - Newton's polynomial interpolation algo implementation by Stefan-Alin Pascaru
+
+void polynomNewtIntr()
+{
+    float p[10000], f=arr[0], f1=1, f2=0;
+    int k=numElem-1,i,j=1;
+ 
+    do
+    {
+        for (i=0;i<numElem-1;i++)
+        {
+            p[i] = ((arr[i+1]-arr[i])/j);
+            arr[i]=p[i];
+        }
+        f1=1;
+        for(i=0;i<j;i++)
+            {
+                f1*=(k-i+1);
+            }
+        f2+=(arr[0]*f1);
+        numElem--;
+        j++;
+    }
+ 	while(numElem!=1);
+    f+=f2;
+    printf("Newton Polynomial result:\t\t\t\t\t       %f", f);
+}
